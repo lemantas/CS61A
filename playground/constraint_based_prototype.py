d0eacd95cf195875f1306355167d651ca92c3fdb
Mantas Levinas <mantas.levinas@gmail.com>
2018-01-16 21:12:48 +0200

    My wife has a pressure on me. Going to spend some time with her

--- playground/constraint_based_prototype.py
+++ playground/constraint_based_prototype.py
@@ -85,7 +85,7 @@ def converter(c, f):
     u, v, w, x, y = [connector() for _ in range(5)]
     multiplier(c, w, u)
     multiplier(v, x, u)
-    adder(v,y, f)
+    adder(v, y, f)
     constant(w, 9)
     constant(x, 5)
     constant(y, 32)
