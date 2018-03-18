5f24982d0a2890cce86a3b38bb1b67a6f6a5c7f8
Mantas Levinas <mantas.levinas@gmail.com>
2018-03-18 22:43:52 +0200

    lab11 in process

--- lab/lab11/lab11.py
+++ lab/lab11/lab11.py
@@ -24,12 +24,19 @@ class IteratorRestart:
     """
     def __init__(self, start, end):
         "*** YOUR CODE HERE ***"
-
+        self.start = start
+        self.end = end
+        self.point = start
     def __next__(self):
         "*** YOUR CODE HERE ***"
-
+        self.point += 1
+        if self.point > self.end:
+            raise StopIteration
+        return self.point
+        
     def __iter__(self):
         "*** YOUR CODE HERE ***"
+        return IteratorRestart(self.start, self.end)
 
 ##############
 # Generators #
