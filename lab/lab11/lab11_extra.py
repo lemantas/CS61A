1d83c5a3d93d3fc9a193b291988770f1d3707522
Mantas Levinas <mantas.levinas@gmail.com>
2018-03-20 22:10:42 +0200

    infinate Streams

--- lab/lab11/lab11_extra.py
+++ lab/lab11/lab11_extra.py
@@ -46,6 +46,9 @@ def add_streams(s1, s2):
     14
     """
     "*** YOUR CODE HERE ***"
+    def compute_rest():
+        return add_streams(s1.rest, s2.rest)
+    return Stream(s1.first + s2.first, compute_rest)
 
 def make_fib_stream():
     """Return a stream containing the Fib sequence.
@@ -59,6 +62,11 @@ def make_fib_stream():
     3
     """
     "*** YOUR CODE HERE ***"
+    def helper(a, b):
+        def compute_rest():
+            return helper(b, a+b)
+        return Stream(a, compute_rest)
+    return helper(0, 1)
 
 def filter_stream(filter_func, stream):
     def make_filtered_rest():
@@ -86,3 +94,8 @@ def interleave(stream1, stream2):
     1
     """     
     "*** YOUR CODE HERE ***"
+    if stream1.first == Stream.empty:
+        return Stream.empty
+    def compute_rest():
+        return interleave(stream2, stream1.rest)
+    return Stream(stream1.first, compute_rest)
