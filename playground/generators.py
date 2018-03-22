3b96b88966f548560dce6a6f72240a512e784556
Mantas Levinas <mantas.levinas@gmail.com>
2018-03-22 21:41:14 +0200

    uns, dos, tres -IterCombiner

--- playground/generators.py
+++ playground/generators.py
@@ -9,15 +9,26 @@ class Naturals():
         
     def __iter__(self):
         return self
+
+from operator import add
         
 class IterCombiner(object):
+    """
+    >>> comb = IterCombiner(Naturals(), Naturals(), add)
+    >>> comb_iter = iter(comb)
+    >>> next(comb_iter)
+    0
+    >>> next(comb_iter)
+    2
+    """
     def __init__(self, iter1, iter2, combiner):
-        self.iter1 = iter1
-        self.iter2 = iter2
+        self.iter1 = iter(iter1)
+        self.iter2 = iter(iter2)
         self.combiner = combiner
     
     def __next__(self):
-        yield self.combainer(next(self.iter1), next(self.iter2))
+        res = self.combiner(next(self.iter1), next(self.iter2))
+        return res
     
     def __iter__(self):
         return self
